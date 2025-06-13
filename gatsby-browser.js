d5c7e6631432f3f5abff30a1274d02dd5335a28d
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-06-13 14:26:04 +0900

    Fix: 페이지 로드할 때 흰색 화면이 잠깐 나타나는 현상 수정

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,20 @@
 require('typeface-montserrat');
+
+const getInitialColorMode = () => {
+  const persistedColorMode = window.localStorage.getItem('isDarkMode');
+  const hasPersistedColorMode = typeof persistedColorMode === 'string';
+  // If the user has explicitly chosen a theme
+  if (hasPersistedColorMode) {
+    return JSON.parse(persistedColorMode) ? 'dark' : 'light';
+  }
+  // If they haven't been explicit, check the media query
+  const mql = window.matchMedia('(prefers-color-scheme: dark)');
+  const hasMediaQueryPreference = typeof mql.matches === 'boolean';
+  if (hasMediaQueryPreference) {
+    return mql.matches ? 'dark' : 'light';
+  }
+  // default to dark mode
+  return 'dark';
+};
+
+document.documentElement.setAttribute('data-theme', getInitialColorMode());
